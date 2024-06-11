9068bb78e53646d54d28b0459d00858fe7d3e780
kadirtikil <kadirtikil@gmail.com>
2024-06-11 10:14:26 +0200

    minor changes. should be gucci for now

--- WSA/WSA/main.cpp
+++ WSA/WSA/main.cpp
@@ -12,17 +12,23 @@ int main() {
 	// Assuming Data is already defined as a template class
 
 	// Declare an object of Data class with appropriate types
+	// The data is immutable after its declaration. So it cannot be changes during run time
 	Data<int, int>* dataElement = new Data<int, int>(1, 2);
 	Data<int, int>* dataElement1 = new Data<int, int>(3, 4);
 	Data<int, int>* dataElement2 = new Data<int, int>(5, 6);
 	dataElement->printData();
 
 	// Declare an object of DataStruc class with appropriate types
+	// The datastructure can be changed in its structure but not in its values.
 	DataStruc<int,int>* myDataStruc = new DataStruc<int, int>();
 	myDataStruc->addDataToList(dataElement);
 	myDataStruc->addDataToList(dataElement1);
 	myDataStruc->addDataToList(dataElement2);
 	myDataStruc->printAll();
+	// Next, the datastructure will take in a string, and iterate through it. 
+	// While doing that, the algorithm will create new and appropriate elements for 
+	// datastruc.
+
 
 	return 0;
 }
